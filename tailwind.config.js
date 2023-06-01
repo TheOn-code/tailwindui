a194626e71f4fa879c085b7a7f2e87b02eb7458b
TheOn-code <prras28@gmail.com>
2023-05-31 23:31:37 -0500

    full project

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+	content: ['./*.html'],
+	theme: {
+		screens: {
+			sm: '480px',
+			md: '768px',
+			lg: '976px',
+			xl: '1440px',
+		},
+		extend: {
+			colors: {
+				brightGreen: 'hsl(131, 27%, 45%)',
+				brightRedLight: 'hsl(131, 35%, 57%)',
+				brightRedSupLight: 'hsl(131, 100%, 79%)',
+				darkBlue: 'hsl(228, 39%, 23%)',
+				darkGrayishBlue: 'hsl(227, 12%, 61%)',
+				veryDarkBlue: 'hsl(233, 12%, 13%)',
+				veryPaleRed: 'hsl(13, 100%, 96%)',
+				veryLightGray: 'hsl(0, 0%, 98%)',
+			},
+		},
+	},
+	plugins: [],
+};
